5639555a1abdbdac039cbf2355d8c3d4c473d150
Yuri Muniz <y.munizgonzalez@gmail.com>
2024-12-10 09:40:32 -0300

    fix deploy

--- src/infra/http/controllers/report/create-report.controller.ts
+++ src/infra/http/controllers/report/create-report.controller.ts
@@ -12,7 +12,7 @@ import { Roles } from '@/infra/auth/roles.decorator'
 import { z } from 'zod'
 import { CreateReportDto } from './dto/create-report-dto'
 import { ReportAlreadyExistsError } from '@/domain/project/application/use-cases/errors/report-already-exists-error'
-import { CreateReportUseCase } from '@/domain/project/application/use-cases/create-Report'
+import { CreateReportUseCase } from '@/domain/project/application/use-cases/create-report'
 
 const createReportBodySchema = z.object({
   name: z.string(),
