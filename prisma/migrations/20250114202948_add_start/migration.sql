0b311a166b676d3b880587b74de0f3a2131b3868
Yuri Muniz <y.munizgonzalez@gmail.com>
2025-01-16 19:08:24 -0300

    fix

--- prisma/migrations/20250114202948_add_start/migration.sql
+++ prisma/migrations/20250114202948_add_start/migration.sql
@@ -1,8 +1,2 @@
 -- AlterTable
-ALTER TABLE "_ProjectToTag" ADD CONSTRAINT "_ProjectToTag_AB_pkey" PRIMARY KEY ("A", "B");
-
--- DropIndex
-DROP INDEX "_ProjectToTag_AB_unique";
-
--- AlterTable
 ALTER TABLE "projects" ADD COLUMN     "start" TIMESTAMP(3);
